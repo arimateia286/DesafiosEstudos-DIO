497eb2efb932fb4f2d0170a2fec02fd176e566f8
arimateia286 <arimateia286@gmail.com>
2024-05-26 13:59:29 -0300

    Adicionado a classe principal ContaTerminal

---
+++ ContaBanco/src/ari/edu/contabanco/ContaTerminal.java
@@ -0,0 +1,36 @@
+package ari.edu.contabanco;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("# Olá, seja muito bem-vindo ao AriBank!\n" +
+        "# Eu me chamo ArIA e vou te ajudar na criação de sua conta.\n" +
+        "# Primeiro, me fala aí seu nome.\n");
+        String nomeCliente = entrada.nextLine();
+        
+        System.out.print("\n# Certo, " + nomeCliente + ". Agora me informa o número da sua agência e logo em seguida o número da conta.\n" +
+        "- Agência (Ex.: 123-4): ");
+        String numeroAgencia = entrada.nextLine();
+        
+        System.out.print("- Conta (Ex.: 1234): ");
+        int numeroConta = entrada.nextInt();
+        
+        System.out.print("\n# Ok, já confirmei aqui no sistema. Sua conta AriBank foi criada com sucesso!\n" +
+        "# Para ativar sua conta é necessário que seja feito um primeiro depósito.\n" +
+        "# Informe, por favor, quanto deseja depositar.\n" +
+        "- Depósito (Ex.: 100.00): ");
+        double saldoConta = entrada.nextDouble();
+        
+        System.out.print("\n# Prontinho, " + nomeCliente + ". Muito obrigado por escolher o AriBank!\n" +
+        "# Seus dados estão logo abaixo:\n" +
+        "- Agência: " + numeroAgencia +
+        "\n- Conta: " + numeroConta +
+        "\n- Saldo: R$" + saldoConta +
+        "\n# Até mais!");
+    }
+    
+}
