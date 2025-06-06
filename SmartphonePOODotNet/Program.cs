49c91cd1ce879e612e28e9544b47db7db22d74f5
arimateia286 <arimateia286@gmail.com>
2025-06-06 15:42:50 -0300

    Adicionado o projeto/desafio de Smartphone com POO em .NET

---
+++ SmartphonePOODotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("99912345678", "Nokia 3310", "123456789012345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Bounce Tales");
+
+Console.WriteLine("\niPhone:");
+Smartphone iphone = new Iphone("99912345678", "iPhone 12", "987654321098765", 128);
+iphone.Numero = "99987654321";
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iTunes");
